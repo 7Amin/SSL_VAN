0743cdcb19f2391af2bc7c520bb82494c4396464
amin.karimi <amin.karimi1992@gmail.com>
2023-02-26 17:56:38 -0500

    add file

--- pretrain/data_loader/data_utils.py
+++ pretrain/data_loader/data_utils.py
@@ -18,18 +18,18 @@ from monai.transforms import (
 
 def get_loader(args):
     splits1 = "/dataset_LUNA16_0.json"
-    splits2 = "/dataset_TCIAcovid19_0.json"
+    # splits2 = "/dataset_TCIAcovid19_0.json"
     splits3 = "/dataset_HNSCC_0.json"
     splits4 = "/dataset_TCIAcolon_v2_0.json"
     splits5 = "/dataset_LIDC_0.json"
     list_dir = "./jsons"
     jsonlist1 = list_dir + splits1
-    jsonlist2 = list_dir + splits2
+    # jsonlist2 = list_dir + splits2
     jsonlist3 = list_dir + splits3
     jsonlist4 = list_dir + splits4
     jsonlist5 = list_dir + splits5
     datadir1 = "/dataset/dataset1"
-    datadir2 = "/dataset/dataset2"
+    # datadir2 = "/dataset/dataset2"
     datadir3 = "/dataset/dataset3"
     datadir4 = "/dataset/dataset4"
     datadir5 = "/dataset/dataset8"
@@ -40,8 +40,8 @@ def get_loader(args):
     for item in datalist1:
         item_dict = {"image": item["image"]}
         new_datalist1.append(item_dict)
-    datalist2 = load_decathlon_datalist(jsonlist2, False, "training", base_dir=datadir2)
-    print("Dataset 2 Covid 19: number of data: {}".format(len(datalist2)))
+    # datalist2 = load_decathlon_datalist(jsonlist2, False, "training", base_dir=datadir2)
+    # print("Dataset 2 Covid 19: number of data: {}".format(len(datalist2)))
     datalist3 = load_decathlon_datalist(jsonlist3, False, "training", base_dir=datadir3)
     print("Dataset 3 HNSCC: number of data: {}".format(len(datalist3)))
     datalist4 = load_decathlon_datalist(jsonlist4, False, "training", base_dir=datadir4)
@@ -49,12 +49,15 @@ def get_loader(args):
     datalist5 = load_decathlon_datalist(jsonlist5, False, "training", base_dir=datadir5)
     print("Dataset 5: number of data: {}".format(len(datalist5)))
     vallist1 = load_decathlon_datalist(jsonlist1, False, "validation", base_dir=datadir1)
-    vallist2 = load_decathlon_datalist(jsonlist2, False, "validation", base_dir=datadir2)
+    # vallist2 = load_decathlon_datalist(jsonlist2, False, "validation", base_dir=datadir2)
     vallist3 = load_decathlon_datalist(jsonlist3, False, "validation", base_dir=datadir3)
     vallist4 = load_decathlon_datalist(jsonlist4, False, "validation", base_dir=datadir4)
     vallist5 = load_decathlon_datalist(jsonlist5, False, "validation", base_dir=datadir5)
-    datalist = new_datalist1 + datalist2 + datalist3 + datalist4 + datalist5
-    val_files = vallist1 + vallist2 + vallist3 + vallist4 + vallist5
+    # datalist = new_datalist1 + datalist2 + datalist3 + datalist4 + datalist5
+    datalist = new_datalist1 + datalist3 + datalist4 + datalist5
+    # val_files = vallist1 + vallist2 + vallist3 + vallist4 + vallist5
+    val_files = vallist1 + vallist3 + vallist4 + vallist5
+
     print("Dataset all training: number of data: {}".format(len(datalist)))
     print("Dataset all validation: number of data: {}".format(len(val_files)))
 
