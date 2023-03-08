694ce72588c97653c077c76ca88547763a4f437c
amin.karimi <amin.karimi1992@gmail.com>
2023-03-08 17:53:23 -0500

    fix segmentor job

--- nodule_segmentor/jobs/job.sh
+++ nodule_segmentor/jobs/job.sh
@@ -12,6 +12,6 @@ module spider cuda
 conda activate ssl_van_seg
 #pip3 install -r ./requirements.txt
 
-PYTHONPATH=. python3 nodule_segmentor/main.py --num_workers 2 \
+PYTHONPATH=. python3 nodule_segmentor/main.py --num_workers 0 \
  --batch_size 1 --base_data ../images --luna_data /luna16 --base_dir_code ./
 
