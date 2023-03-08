db983408bc63da669712d09a5f166ea0277f88a3
amin.karimi <amin.karimi1992@gmail.com>
2023-03-08 16:09:44 -0500

    Dataset and Loader is finished

--- nodule_segmentor/jobs/job.sh
+++ nodule_segmentor/jobs/job.sh
@@ -10,7 +10,7 @@
 cd SSL_VAN
 module spider cuda
 conda activate ssl_van_seg
-pip3 install -r ./requirements.txt
+#pip3 install -r ./requirements.txt
 
 PYTHONPATH=.. python3 nodule_segmentor/main.py --num_workers 6 \
  --batch_size 2 --base_data ../images --luna_data /luna16 --base_dir_code ../ \
