34614f9e0e19de13df7ad8400deda22aaa8e0dcf
amin.karimi <amin.karimi1992@gmail.com>
2023-03-08 21:02:31 -0500

    fix segmentor job

--- nodule_segmentor/data_loader/data_utils.py
+++ nodule_segmentor/data_loader/data_utils.py
@@ -2,6 +2,23 @@ import torch
 from torch.utils.data import DataLoader
 from torch.utils.data.distributed import DistributedSampler
 from nodule_segmentor.data_loader.luna16_dataset import Luna16Dataset
+from random import randint
+
+
+def random_patch_collate(batch, patch_size):
+    patches = []
+    for vol in batch:
+        # Choose a random patch from the volume
+        x = randint(0, vol.shape[2] - patch_size)
+        y = randint(0, vol.shape[3] - patch_size)
+        z = randint(0, vol.shape[4] - patch_size)
+        patch = vol[:, :, x:x+patch_size, y:y+patch_size, z:z+patch_size]
+        patches.append(patch)
+
+    # Stack the patches into a batch tensor
+    batch_tensor = torch.stack(patches)
+
+    return batch_tensor
 
 
 def get_loader(args, data_type="training"):
@@ -13,7 +30,7 @@ def get_loader(args, data_type="training"):
     shuffle = False
     if data_type == "training":
         shuffle = True
-    data_loader = DataLoader(dataset, batch_size=args.batch_size, num_workers=num_workers,
-                             drop_last=True, shuffle=shuffle)
+    data_loader = DataLoader(dataset, batch_size=args.batch_size, num_workers=num_workers, drop_last=True,
+                             shuffle=shuffle, collate_fn=lambda x: random_patch_collate(x, patch_size=args.patch_size))
 
     return data_loader
--- nodule_segmentor/jobs/job.sh
+++ nodule_segmentor/jobs/job.sh
@@ -13,5 +13,5 @@ conda activate ssl_van_seg
 #pip3 install -r ./requirements.txt
 
 PYTHONPATH=. python3 nodule_segmentor/main.py --num_workers 0 \
- --batch_size 1 --base_data ../images --luna_data /luna16 --base_dir_code ./
+ --batch_size 1 --base_data ../images --luna_data /luna16 --base_dir_code ./ --patch_size 128
 
