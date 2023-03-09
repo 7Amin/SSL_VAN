ea2bed42e66e6c19c6ca96d231e1568137b95c93
amin.karimi <amin.karimi1992@gmail.com>
2023-03-08 21:42:00 -0500

    fix segmentor job

--- nodule_segmentor/losses/loss.py
+++ nodule_segmentor/losses/loss.py
@@ -7,7 +7,9 @@ class Loss(torch.nn.Module):
         self.recon_loss = torch.nn.CrossEntropyLoss().to(args.device)
 
     def __call__(self, output_recons, target_recons):
-        _, d, _, _ = target_recons.shape
+        # _, d, _, _ = target_recons.shape
 
-        recon_loss = self.recon_loss(output_recons[:, 0, : d, :, :], target_recons)
+        # recon_loss = self.recon_loss(output_recons[:, : d, :, :], target_recons)
+        recon_loss = self.recon_loss(output_recons, target_recons)
+
         return recon_loss
