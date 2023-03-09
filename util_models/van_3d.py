5e15488c5cb7e84ded605a0ef6ff1eb670d45c8c
amin.karimi <amin.karimi1992@gmail.com>
2023-03-08 21:30:13 -0500

    fix segmentor job

--- util_models/van_3d.py
+++ util_models/van_3d.py
@@ -58,12 +58,12 @@ class Block3D(nn.Module):
                  norm_cfg=dict(type='SyncBN', requires_grad=True)):
         super().__init__()
         # self.norm1 = build_norm_layer(norm_cfg, dim)[1]
-        self.norm1 = nn.BatchNorm3d(1)
+        self.norm1 = nn.BatchNorm3d(dim)
         self.attn = SpatialAttention3D(dim)
         self.drop_path = DropPath(drop_path) if drop_path > 0. else nn.Identity()
 
         # self.norm2 = build_norm_layer(norm_cfg, dim)[1]
-        self.norm2 = nn.BatchNorm3d(1)
+        self.norm2 = nn.BatchNorm3d(dim)
         mlp_hidden_dim = int(dim * mlp_ratio)
         self.mlp = Mlp(in_features=dim, hidden_features=mlp_hidden_dim,
                        act_layer=act_layer, drop=drop, linear=linear)
