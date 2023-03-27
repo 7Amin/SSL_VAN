6068b845a5ca6f12c30cc941dcc2fd5ea214fbca
amin.karimi <amin.karimi1992@gmail.com>
2023-03-27 01:38:42 -0400

    fix some problems

--- BTCV/jobs/job_devel.sh
+++ BTCV/jobs/job_devel.sh
@@ -13,7 +13,7 @@ conda activate ssl_van_seg
 #pip3 install -r ./requirements.txt
 
 PYTHONPATH=. python3 BTCV/main.py  --workers 8 --base_data ../images/BTCV/Abdomen/RawData/Training \
---json_list input_list/dataset_BTCV_List.json --noamp --save_checkpoint --max_epochs 6500 \
+--json_list input_list/dataset_BTCV_List.json --noamp --save_checkpoint --max_epochs 10000 \
  --use_normal_dataset --batch_size 2 --num_stages 4 --embed_dims 64 128 256 512 --depths 3 4 6 3 \
 --mlp_ratios 8 8 4 4 --val_every 5 --checkpoint
 
