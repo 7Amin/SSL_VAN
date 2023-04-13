6b99c7bc781ff350a0ec6bac770634c7388de509
amin.karimi <amin.karimi1992@gmail.com>
2023-04-12 22:24:44 -0400

    change size of model

--- BRATS21/jobs/job_main_gpu.sh
+++ BRATS21/jobs/job_main_gpu.sh
@@ -27,4 +27,4 @@ PYTHONPATH=. python3 BRATS21/main.py --workers 8 --data_dir ../images/BraTS21 \
 #PYTHONPATH=. python3 BTCV/main.py  --workers 8 --base_data ../images/BTCV/Abdomen/RawData/Training \
 #--json_list input_list/dataset_BTCV_List.json --save_checkpoint --max_epochs 15000 \
 #--distributed --use_normal_dataset --batch_size 8 --num_stages 4 --embed_dims 96 192 480 768 --depths 3 3 24 3 \
-#--mlp_ratios 8 8 4 4 --roi_x 96 --roi_y 96 --roi_z 96 --val_every 5 --checkpoint
+#--mlp_ratios 8 8 4 4 --roi_x 96 --roi_y 96 --roi_z 96 --val_every 5
