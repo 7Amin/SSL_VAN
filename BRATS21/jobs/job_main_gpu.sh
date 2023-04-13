533ee33c7dd7e00244b99c79e5cfa0da15d51107
amin.karimi <amin.karimi1992@gmail.com>
2023-04-12 22:26:56 -0400

    change size of model

--- BRATS21/jobs/job_main_gpu.sh
+++ BRATS21/jobs/job_main_gpu.sh
@@ -18,13 +18,13 @@ conda activate ssl_van_seg
 #--distributed --use_normal_dataset --batch_size 8 --num_stages 4 --embed_dims 64 128 256 512 --depths 3 4 6 3 \
 #--mlp_ratios 8 8 4 4 --fold=0 --roi_x 96 --roi_y 96 --roi_z 96 --squared_dice --val_every 5 --checkpoint
 
-#PYTHONPATH=. python3 BRATS21/main.py --workers 8 --data_dir ../images/BraTS21 \
-#--json_list input_list/dataset_BRATS21_List.json --save_checkpoint --max_epochs 15000 \
-#--distributed --use_normal_dataset --batch_size 8 --num_stages 4 --embed_dims 96 192 384 768 --depths 6 6 90 6 \
-#--mlp_ratios 8 8 4 4 --fold=0 --roi_x 96 --roi_y 96 --roi_z 96 --squared_dice --val_every 5 --checkpoint
-
-
 PYTHONPATH=. python3 BRATS21/main.py --workers 8 --data_dir ../images/BraTS21 \
 --json_list input_list/dataset_BRATS21_List.json --save_checkpoint --max_epochs 15000 \
---distributed --use_normal_dataset --batch_size 8 --num_stages 4 --embed_dims 96 192 480 768 --depths 3 3 24 3 \
+--distributed --use_normal_dataset --batch_size 8 --num_stages 4 --embed_dims 96 192 384 768 --depths 6 6 90 6 \
 --mlp_ratios 8 8 4 4 --fold=0 --roi_x 96 --roi_y 96 --roi_z 96 --squared_dice --val_every 5 --checkpoint
+
+
+#PYTHONPATH=. python3 BRATS21/main.py --workers 8 --data_dir ../images/BraTS21 \
+#--json_list input_list/dataset_BRATS21_List.json --save_checkpoint --max_epochs 15000 \
+#--distributed --use_normal_dataset --batch_size 8 --num_stages 4 --embed_dims 96 192 480 768 --depths 3 3 24 3 \
+#--mlp_ratios 8 8 4 4 --fold=0 --roi_x 96 --roi_y 96 --roi_z 96 --squared_dice --val_every 5 --checkpoint
