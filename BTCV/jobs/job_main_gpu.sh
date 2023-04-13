977179179dd1705f53c751d4a988d13c60b78869
amin.karimi <amin.karimi1992@gmail.com>
2023-04-12 22:26:41 -0400

    change size of model

--- BTCV/jobs/job_main_gpu.sh
+++ BTCV/jobs/job_main_gpu.sh
@@ -16,12 +16,12 @@ conda activate ssl_van_seg
 #PYTHONPATH=. python3 BTCV/main.py  --workers 8 --base_data ../images/BTCV/Abdomen/RawData/Training \
 #--json_list input_list/dataset_BTCV_List.json --save_checkpoint --max_epochs 15000 \
 #--distributed --use_normal_dataset --batch_size 8 --num_stages 4 --embed_dims 64 128 256 512 --depths 3 4 6 3 \
-#--mlp_ratios 8 8 4 4 --roi_x 96 --roi_y 96 --roi_z 96 --checkpoint --val_every 5
+#--mlp_ratios 8 8 4 4 --roi_x 96 --roi_y 96 --roi_z 96 --val_every 5 --checkpoint
 
 PYTHONPATH=. python3 BTCV/main.py  --workers 8 --base_data ../images/BTCV/Abdomen/RawData/Training \
 --json_list input_list/dataset_BTCV_List.json --save_checkpoint --max_epochs 15000 \
 --distributed --use_normal_dataset --batch_size 8 --num_stages 4 --embed_dims 96 192 384 768 --depths 6 6 90 6 \
---mlp_ratios 8 8 4 4 --roi_x 96 --roi_y 96 --roi_z 96 --val_every 5
+--mlp_ratios 8 8 4 4 --roi_x 96 --roi_y 96 --roi_z 96 --val_every 5 --checkpoint
 
 #PYTHONPATH=. python3 BTCV/main.py  --workers 8 --base_data ../images/BTCV/Abdomen/RawData/Training \
 #--json_list input_list/dataset_BTCV_List.json --save_checkpoint --max_epochs 15000 \
