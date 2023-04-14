f3dbda0b4b9805184aeea2305d628e877a328d82
amin.karimi <amin.karimi1992@gmail.com>
2023-04-14 08:02:27 -0400

    change log locations and edit jobs

--- BRATS21/jobs/job_main_gpu.sh
+++ BRATS21/jobs/job_main_gpu.sh
@@ -20,8 +20,8 @@ conda activate ssl_van_seg
 
 PYTHONPATH=. python3 BRATS21/main.py --workers 8 --data_dir ../images/BraTS21 \
 --json_list input_list/dataset_BRATS21_List.json --save_checkpoint --max_epochs 15000 \
---distributed --use_normal_dataset --batch_size 8 --num_stages 4 --embed_dims 96 192 384 768 --depths 6 6 90 6 \
---mlp_ratios 8 8 4 4 --fold=0 --roi_x 96 --roi_y 96 --roi_z 96 --squared_dice --val_every 5 --checkpoint
+--distributed --use_normal_dataset --batch_size 2 --num_stages 4 --embed_dims 96 192 384 768 --depths 6 6 90 6 \
+--mlp_ratios 8 8 4 4 --fold=0 --roi_x 96 --roi_y 96 --roi_z 96 --squared_dice --val_every 5
 
 
 #PYTHONPATH=. python3 BRATS21/main.py --workers 8 --data_dir ../images/BraTS21 \
--- BTCV/jobs/job_main_gpu.sh
+++ BTCV/jobs/job_main_gpu.sh
@@ -20,8 +20,8 @@ conda activate ssl_van_seg
 
 PYTHONPATH=. python3 BTCV/main.py  --workers 8 --base_data ../images/BTCV/Abdomen/RawData/Training \
 --json_list input_list/dataset_BTCV_List.json --save_checkpoint --max_epochs 15000 \
---distributed --use_normal_dataset --batch_size 8 --num_stages 4 --embed_dims 96 192 384 768 --depths 6 6 90 6 \
---mlp_ratios 8 8 4 4 --roi_x 96 --roi_y 96 --roi_z 96 --val_every 5 --checkpoint
+--distributed --use_normal_dataset --batch_size 2 --num_stages 4 --embed_dims 96 192 384 768 --depths 6 6 90 6 \
+--mlp_ratios 8 8 4 4 --roi_x 96 --roi_y 96 --roi_z 96 --val_every 5
 
 #PYTHONPATH=. python3 BTCV/main.py  --workers 8 --base_data ../images/BTCV/Abdomen/RawData/Training \
 #--json_list input_list/dataset_BTCV_List.json --save_checkpoint --max_epochs 15000 \
