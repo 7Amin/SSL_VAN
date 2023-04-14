6e73ed9b656543c4ba64c44a34d0d1245940a34e
amin.karimi <amin.karimi1992@gmail.com>
2023-04-14 08:05:56 -0400

    change log locations and edit jobs

--- BRATS21/jobs/job_main_gpu.sh
+++ BRATS21/jobs/job_main_gpu.sh
@@ -13,15 +13,15 @@ module spider cuda
 conda activate ssl_van_seg
 #pip3 install -r ./requirements.txt
 
-#PYTHONPATH=. python3 BRATS21/main.py --workers 8 --data_dir ../images/BraTS21 \
-#--json_list input_list/dataset_BRATS21_List.json --save_checkpoint --max_epochs 15000 \
-#--distributed --use_normal_dataset --batch_size 8 --num_stages 4 --embed_dims 64 128 256 512 --depths 3 4 6 3 \
-#--mlp_ratios 8 8 4 4 --fold=0 --roi_x 96 --roi_y 96 --roi_z 96 --squared_dice --val_every 5 --checkpoint
-
 PYTHONPATH=. python3 BRATS21/main.py --workers 8 --data_dir ../images/BraTS21 \
 --json_list input_list/dataset_BRATS21_List.json --save_checkpoint --max_epochs 15000 \
---distributed --use_normal_dataset --batch_size 2 --num_stages 4 --embed_dims 96 192 384 768 --depths 6 6 90 6 \
---mlp_ratios 8 8 4 4 --fold=0 --roi_x 96 --roi_y 96 --roi_z 96 --squared_dice --val_every 5
+--distributed --use_normal_dataset --batch_size 8 --num_stages 4 --embed_dims 64 128 256 512 --depths 3 4 6 3 \
+--mlp_ratios 8 8 4 4 --fold=0 --roi_x 96 --roi_y 96 --roi_z 96 --squared_dice --val_every 5 --checkpoint
+
+#PYTHONPATH=. python3 BRATS21/main.py --workers 8 --data_dir ../images/BraTS21 \
+#--json_list input_list/dataset_BRATS21_List.json --save_checkpoint --max_epochs 15000 \
+#--distributed --use_normal_dataset --batch_size 2 --num_stages 4 --embed_dims 96 192 384 768 --depths 6 6 90 6 \
+#--mlp_ratios 8 8 4 4 --fold=0 --roi_x 96 --roi_y 96 --roi_z 96 --squared_dice --val_every 5 --checkpoint
 
 
 #PYTHONPATH=. python3 BRATS21/main.py --workers 8 --data_dir ../images/BraTS21 \
