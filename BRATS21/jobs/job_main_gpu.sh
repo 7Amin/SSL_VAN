7b57c66b99bb5201e59fb5a7de9a76623670b299
amin.karimi <amin.karimi1992@gmail.com>
2023-04-17 09:11:32 -0400

    edit job

--- BRATS21/jobs/job_main_gpu.sh
+++ BRATS21/jobs/job_main_gpu.sh
@@ -13,10 +13,10 @@ module spider cuda
 conda activate ssl_van_seg
 #pip3 install -r ./requirements.txt
 
-PYTHONPATH=. python3 BRATS21/main.py --workers 8 --data_dir ../images/BraTS21 \
---json_list input_list/dataset_BRATS21_List.json --save_checkpoint --max_epochs 15000 \
---distributed --use_normal_dataset --batch_size 8 --num_stages 4 --embed_dims 64 128 256 512 --depths 3 4 6 3 \
---mlp_ratios 8 8 4 4 --fold=0 --roi_x 96 --roi_y 96 --roi_z 96 --squared_dice --val_every 5
+#PYTHONPATH=. python3 BRATS21/main.py --workers 8 --data_dir ../images/BraTS21 \
+#--json_list input_list/dataset_BRATS21_List.json --save_checkpoint --max_epochs 15000 \
+#--distributed --use_normal_dataset --batch_size 8 --num_stages 4 --embed_dims 64 128 256 512 --depths 3 4 6 3 \
+#--mlp_ratios 8 8 4 4 --fold=0 --roi_x 96 --roi_y 96 --roi_z 96 --squared_dice --val_every 5 --checkpoint
 
 #PYTHONPATH=. python3 BRATS21/main.py --workers 8 --data_dir ../images/BraTS21 \
 #--json_list input_list/dataset_BRATS21_List.json --save_checkpoint --max_epochs 15000 \
@@ -24,7 +24,7 @@ PYTHONPATH=. python3 BRATS21/main.py --workers 8 --data_dir ../images/BraTS21 \
 #--mlp_ratios 8 8 4 4 --fold=0 --roi_x 96 --roi_y 96 --roi_z 96 --squared_dice --val_every 5 --checkpoint
 
 
-#PYTHONPATH=. python3 BRATS21/main.py --workers 8 --data_dir ../images/BraTS21 \
-#--json_list input_list/dataset_BRATS21_List.json --save_checkpoint --max_epochs 15000 \
-#--distributed --use_normal_dataset --batch_size 8 --num_stages 4 --embed_dims 96 192 480 768 --depths 3 3 24 3 \
-#--mlp_ratios 8 8 4 4 --fold=0 --roi_x 96 --roi_y 96 --roi_z 96 --squared_dice --val_every 5  --checkpoint
+PYTHONPATH=. python3 BRATS21/main.py --workers 8 --data_dir ../images/BraTS21 \
+--json_list input_list/dataset_BRATS21_List.json --save_checkpoint --max_epochs 15000 \
+--distributed --use_normal_dataset --batch_size 8 --num_stages 4 --embed_dims 96 192 480 768 --depths 3 3 24 3 \
+--mlp_ratios 8 8 4 4 --fold=0 --roi_x 96 --roi_y 96 --roi_z 96 --squared_dice --val_every 5  --checkpoint
