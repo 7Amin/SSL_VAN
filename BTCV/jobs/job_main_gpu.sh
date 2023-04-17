37a4903aa6c76d20eac16ff121b7658735ce3c02
amin.karimi <amin.karimi1992@gmail.com>
2023-04-17 09:08:06 -0400

    fix a bug

--- BTCV/jobs/job_main_gpu.sh
+++ BTCV/jobs/job_main_gpu.sh
@@ -26,4 +26,4 @@ conda activate ssl_van_seg
 PYTHONPATH=. python3 BTCV/main.py  --workers 8 --base_data ../images/BTCV/Abdomen/RawData/Training \
 --json_list input_list/dataset_BTCV_List.json --save_checkpoint --max_epochs 15000 \
 --distributed --use_normal_dataset --batch_size 8 --num_stages 4 --embed_dims 96 192 480 768 --depths 3 3 24 3 \
---mlp_ratios 8 8 4 4 --roi_x 96 --roi_y 96 --roi_z 96 --val_every 5 --checkpoint
+--mlp_ratios 8 8 4 4 --roi_x 96 --roi_y 96 --roi_z 96 --val_every 5
