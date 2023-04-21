cefded7c5633af7a221c1b13b693f25410512fff
amin.karimi <amin.karimi1992@gmail.com>
2023-04-21 00:19:11 -0400

    add model and new loss to cluster part

---
+++ clustering/losses/loss.py
@@ -0,0 +1,79 @@
+import torch
+from torch import nn
+import numpy as np
+
+
+class IoU(torch.nn.Module):
+    def __init__(self, args):
+        super().__init__()
+        self.args = args
+
+    def __call__(self, predictions, targets):
+        return get_IoU(predictions, targets, self.args) * -1.0
+
+
+def get_IoU(predictions, targets, args):
+    predictions = predictions > 0.5
+    targets = targets > 0.5
+
+    # Calculate intersection and union
+    intersection = torch.logical_and(predictions, targets).float().sum()
+    union = torch.logical_or(predictions, targets).float().sum()
+
+    # Calculate IoU, handling the case where the union is 0
+    iou = torch.where(union > 0.0, intersection / union, torch.tensor(0.0).to(args.device))
+
+    return iou
+
+
+class ClusteringLoss(nn.Module):
+    def __init__(self, embedding_dim, max_cluster_size):
+        super(ClusteringLoss, self).__init__()
+        self.max_cluster_size = max_cluster_size
+        self.embedding_dim = embedding_dim
+        self.embedding = nn.Embedding(max_cluster_size, embedding_dim)
+
+    def forward(self, outputs, targets):
+        b, seq, cluster_number = targets.shape
+        targets_new = targets.unsqueeze(-1)
+        targets_new = targets_new.view(b * seq * cluster_number, 1)
+        targets_new = self.embedding(targets_new)
+        targets_new = targets_new.view(b, seq, cluster_number, self.embedding_dim)
+
+        cos_sim = torch.nn.functional.cosine_similarity(outputs, targets_new.unsqueeze(3), dim=-1)
+
+        # apply softmax along the embedding dimension
+        softmax_cos_sim = torch.nn.functional.softmax(cos_sim, dim=-1)
+        # res = softmax_cos_sim[b, seq, cluster_number, targets]
+        res = softmax_cos_sim[np.arange(b)[:, np.newaxis, np.newaxis],
+                              np.arange(seq)[:, np.newaxis],
+                              np.arange(cluster_number),
+                              targets]
+        return res
+
+
+def get_loss(loss_name, args):
+    if loss_name == 'CrossEntropyLoss':
+        return torch.nn.CrossEntropyLoss().to(args.device)
+    if loss_name == 'IoU':
+        return IoU(args).to(args.device)
+    if loss_name == 'L1Loss':
+        return torch.nn.L1Loss().to(args.device)
+    if loss_name == 'MSELoss':
+        return torch.nn.MSELoss().to(args.device)
+
+
+# def test():
+#     embedding_dim = 5
+#     max_cluster_size = 15
+#     b = 4
+#     seq = 96
+#     cluster_number = 25
+#     loss_func = ClusteringLoss(embedding_dim, max_cluster_size)
+#     output_matrix = torch.randn(b, seq, cluster_number, max_cluster_size, embedding_dim)
+#     target_matrix = torch.randint(0, max_cluster_size, (b, seq, cluster_number))
+#     loss_value = loss_func(output_matrix, target_matrix)
+#     print(loss_value)
+#
+#
+# test()
---
+++ clustering/model/van.py
@@ -0,0 +1,62 @@
+import torch
+import torch.nn as nn
+
+from monai.utils import ensure_tuple_rep
+from util_models.van_3d import VAN3D
+
+
+class VAN(nn.Module):
+    def __init__(self, embed_dims, mlp_ratios, depths, num_stages, in_channels, out_channels, dropout_path_rate,
+                 upsample="vae", project_dim=512, image_size=96*96, cluster_number=400, max_cluster_size=500):
+        super(VAN, self).__init__()
+        self.van3d = VAN3D(in_chans=in_channels, drop_path_rate=dropout_path_rate, embed_dims=embed_dims,
+                           mlp_ratios=mlp_ratios, depths=depths, num_stages=num_stages)
+
+        self.rotation_pre = nn.Identity()
+        self.rotation_head = nn.Linear(embed_dims[-1], 4)
+        self.contrastive_pre = nn.Identity()
+        self.contrastive_head = nn.Linear(embed_dims[-1], 512)
+        self.projection = nn.Linear(image_size * out_channels, project_dim * cluster_number * max_cluster_size)
+        self.project_dim = project_dim
+        self.cluster_number = cluster_number
+        self.max_cluster_size = max_cluster_size
+        if upsample == "deconv":
+            self.conv = nn.Sequential(
+                nn.ConvTranspose3d(embed_dims[-1], embed_dims[-1] // 2, kernel_size=(2, 2, 2), stride=(2, 2, 2)),
+                nn.ConvTranspose3d(embed_dims[-1] // 2, embed_dims[-1] // 4, kernel_size=(2, 2, 2), stride=(2, 2, 2)),
+                nn.ConvTranspose3d(embed_dims[-1] // 4, embed_dims[-1] // 8, kernel_size=(2, 2, 2), stride=(2, 2, 2)),
+                nn.ConvTranspose3d(embed_dims[-1] // 8, embed_dims[-1] // 16, kernel_size=(2, 2, 2), stride=(2, 2, 2)),
+                nn.ConvTranspose3d(embed_dims[-1] // 16, out_channels, kernel_size=(2, 2, 2), stride=(2, 2, 2)),
+            )
+        elif upsample == "vae":
+            self.conv = nn.Sequential(
+                nn.Conv3d(embed_dims[-1], embed_dims[-1] // 2, kernel_size=3, stride=1, padding=1),
+                nn.InstanceNorm3d(embed_dims[-1] // 2),
+                nn.LeakyReLU(),
+                nn.Upsample(scale_factor=2, mode="trilinear", align_corners=False),
+                nn.Conv3d(embed_dims[-1] // 2, embed_dims[-1] // 4, kernel_size=3, stride=1, padding=1),
+                nn.InstanceNorm3d(embed_dims[-1] // 4),
+                nn.LeakyReLU(),
+                nn.Upsample(scale_factor=2, mode="trilinear", align_corners=False),
+                nn.Conv3d(embed_dims[-1] // 4, embed_dims[-1] // 8, kernel_size=3, stride=1, padding=1),
+                nn.InstanceNorm3d(embed_dims[-1] // 8),
+                nn.LeakyReLU(),
+                nn.Upsample(scale_factor=2, mode="trilinear", align_corners=False),
+                nn.Conv3d(embed_dims[-1] // 8, embed_dims[-1] // 16, kernel_size=3, stride=1, padding=1),
+                nn.InstanceNorm3d(embed_dims[-1] // 16),
+                nn.LeakyReLU(),
+                nn.Upsample(scale_factor=2, mode="trilinear", align_corners=False),
+                nn.Conv3d(embed_dims[-1] // 16, out_channels, kernel_size=1, stride=1),
+            )
+
+    def forward(self, x):
+        b, seq, c, h, w = x.shape
+        # x = x.view(b, c, seq, h, w)
+        x = self.van3d(x.contiguous())[-1]
+        # x = x.flatten(start_dim=2, end_dim=4)
+        x = self.conv(x)
+        # x = x.squeeze(1)
+        x = x.view(b, seq, c * h * w)
+        y = self.projection(x)
+        y = y.view(b, seq, self.cluster_number, self.max_cluster_size, self.project_dim)
+        return x, y
