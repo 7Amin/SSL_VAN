42aec1886bcffa9db07d8636f509113e968e9449
Tassos Manganaris <tassos.manganaris@gmail.com>
2023-05-17 02:47:37 -0400

    First draft of SimMIM based loader

--- pretrain/utils/data_utils.py
+++ pretrain/utils/data_utils.py
@@ -26,7 +26,8 @@ from monai.transforms import (
     ToTensord,
 )
 
-from simmim.data.data_simmim import build_loader_simmim
+from argparse import Namespace
+from simmim.data.data_simmim import SimMIMTransform
 
 
 def get_loader(args):
@@ -71,61 +72,38 @@ def get_loader(args):
     print("Dataset all training: number of data: {}".format(len(datalist)))
     print("Dataset all validation: number of data: {}".format(len(val_files)))
 
-    train_transforms = Compose(
-        [
-            LoadImaged(keys=["image"]),
-            AddChanneld(keys=["image"]),
-            Orientationd(keys=["image"], axcodes="RAS"),
-            ScaleIntensityRanged(
-                keys=["image"], a_min=args.a_min, a_max=args.a_max, b_min=args.b_min, b_max=args.b_max, clip=True
-            ),
-            SpatialPadd(keys="image", spatial_size=[args.roi_x, args.roi_y, args.roi_z]),
-            CropForegroundd(keys=["image"], source_key="image", k_divisible=[args.roi_x, args.roi_y, args.roi_z]),
-            RandSpatialCropSamplesd(
-                keys=["image"],
-                roi_size=[args.roi_x, args.roi_y, args.roi_z],
-                num_samples=args.sw_batch_size,
-                random_center=True,
-                random_size=False,
-            ),
-            ToTensord(keys=["image"]),
-        ]
-    )
-    val_transforms = Compose(
-        [
-            LoadImaged(keys=["image"]),
-            AddChanneld(keys=["image"]),
-            Orientationd(keys=["image"], axcodes="RAS"),
-            ScaleIntensityRanged(
-                keys=["image"], a_min=args.a_min, a_max=args.a_max, b_min=args.b_min, b_max=args.b_max, clip=True
-            ),
-            SpatialPadd(keys="image", spatial_size=[args.roi_x, args.roi_y, args.roi_z]),
-            CropForegroundd(keys=["image"], source_key="image", k_divisible=[args.roi_x, args.roi_y, args.roi_z]),
-            RandSpatialCropSamplesd(
-                keys=["image"],
-                roi_size=[args.roi_x, args.roi_y, args.roi_z],
-                num_samples=args.sw_batch_size,
-                random_center=True,
-                random_size=False,
-            ),
-            ToTensord(keys=["image"]),
-        ]
-    )
+    config = Namespace(**{
+        "DATA_IMG_SIZE": None,
+        "MODEL": Namespace(**{
+            "TYPE": "vit",
+            "VIT": Namespace(**{
+                "PATCH_SIZE": 16
+            })
+        }),
+        "DATA": Namespace(**{
+            "IMG_SIZE": 96,
+            "MASK_PATCH_SIZE": 32,
+            "MASK_RATIO": 0.6
+        })
+    })
+
+    train_transform = SimMIMTransform(config)
+    val_transform = SimMIMTransform(config)
 
     if args.cache_dataset:
         print("Using MONAI Cache Dataset")
-        train_ds = CacheDataset(data=datalist, transform=train_transforms, cache_rate=0.5, num_workers=num_workers)
+        train_ds = CacheDataset(data=datalist, transform=train_transform, cache_rate=0.5, num_workers=num_workers)
     elif args.smartcache_dataset:
         print("Using MONAI SmartCache Dataset")
         train_ds = SmartCacheDataset(
             data=datalist,
-            transform=train_transforms,
+            transform=train_transform,
             replace_rate=1.0,
             cache_num=2 * args.batch_size * args.sw_batch_size,
         )
     else:
         print("Using generic dataset")
-        train_ds = Dataset(data=datalist, transform=train_transforms)
+        train_ds = Dataset(data=datalist, transform=train_transform)
 
     if args.distributed:
         train_sampler = DistributedSampler(dataset=train_ds, even_divisible=True, shuffle=True)
@@ -135,7 +113,7 @@ def get_loader(args):
         train_ds, batch_size=args.batch_size, num_workers=num_workers, sampler=train_sampler, drop_last=True
     )
 
-    val_ds = Dataset(data=val_files, transform=val_transforms)
+    val_ds = Dataset(data=val_files, transform=val_transform)
     val_loader = DataLoader(val_ds, batch_size=args.batch_size, num_workers=num_workers, shuffle=False, drop_last=True)
 
     return train_loader, val_loader
