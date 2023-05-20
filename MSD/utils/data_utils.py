ddd5ea295030182acd63dc926ce6d85ed3a04b19
amin.karimi <amin.karimi1992@gmail.com>
2023-05-20 12:13:28 -0400

    fix msd data loader

--- MSD/utils/data_utils.py
+++ MSD/utils/data_utils.py
@@ -1,3 +1,4 @@
+import json
 import math
 import os
 
@@ -5,7 +6,7 @@ import numpy as np
 import torch
 
 from monai import data, transforms
-from monai.data import load_decathlon_datalist
+from monai.transforms import AsDiscreted
 
 
 class Sampler(torch.utils.data.Sampler):
@@ -55,31 +56,39 @@ class Sampler(torch.utils.data.Sampler):
         self.epoch = epoch
 
 
+def datafold_read(datalist, basedir, fold=0, task="Task01_BrainTumour"):
+
+    with open(datalist) as f:
+        json_data = json.load(f)
+
+    json_data = json_data[task]
+
+    for d in json_data:
+        for k, v in d.items():
+            if isinstance(d[k], list):
+                d[k] = [os.path.join(basedir, iv) for iv in d[k]]
+            elif isinstance(d[k], str):
+                d[k] = os.path.join(basedir, d[k]) if len(d[k]) > 0 else d[k]
+
+    tr = json_data['training']
+    val = json_data['validation']
+    return tr, val
+
+
 def get_loader(args):
-    task = args.task
-    data_dir = args.base_data + "/" + task
+    data_dir = args.base_data
     datalist_json = args.json_list
-    print(datalist_json)
-    print(task)
+    train_files, validation_files = datafold_read(datalist=datalist_json, basedir=data_dir,
+                                                  fold=args.fold, task=args.task)
     train_transform = transforms.Compose(
         [
             transforms.LoadImaged(keys=["image", "label"]),
-            # add channel to data
-            transforms.AddChanneld(keys=["image", "label"]),
+            transforms.ConvertToMultiChannelBasedOnBratsClassesd(keys="label"),
             transforms.Orientationd(keys=["image", "label"], axcodes="RAS"),
-            # Resamples the loaded image and label data to have the specified voxel spacing.
-            transforms.Spacingd(
-                keys=["image", "label"], pixdim=(args.space_x, args.space_y, args.space_z), mode=("bilinear", "nearest")
-            ),
-            # Scales the intensity values of the image data to the specified range. The clip parameter controls
-            # whether values outside the input intensity range should be clipped or not
-            transforms.ScaleIntensityRanged(
-                keys=["image"], a_min=args.a_min, a_max=args.a_max, b_min=args.b_min, b_max=args.b_max, clip=True
-            ),
-            # Crops the loaded image and label data to the smallest bounding box that contains all foreground
-            # voxels in the image data. This is useful for removing empty regions around the edges of the image
             transforms.CropForegroundd(keys=["image", "label"], source_key="image"),
-            # Randomly crops a spatial region of size.
+            # transforms.RandSpatialCropd(
+            #     keys=["image", "label"], roi_size=[args.roi_x, args.roi_y, args.roi_z], random_size=False
+            # ),
             transforms.RandCropByPosNegLabeld(
                 keys=["image", "label"],
                 spatial_size=(args.roi_x, args.roi_y, args.roi_z),
@@ -90,88 +99,75 @@ def get_loader(args):
                 label_key="label",
                 image_threshold=0,
             ),
-            # Randomly flips the loaded image and label data along the specified spatial axis with a probability
+            AsDiscreted(keys=["label"], threshold_values=True, logit_thresh=0.5, n_classes=args.out_channels),
             transforms.RandFlipd(keys=["image", "label"], prob=args.RandFlipd_prob, spatial_axis=0),
             transforms.RandFlipd(keys=["image", "label"], prob=args.RandFlipd_prob, spatial_axis=1),
             transforms.RandFlipd(keys=["image", "label"], prob=args.RandFlipd_prob, spatial_axis=2),
-            # Randomly rotates the loaded image and label data by a multiple of 90 degrees with a probability
             transforms.RandRotate90d(keys=["image", "label"], prob=args.RandRotate90d_prob, max_k=3),
-            # transform is used to randomly shift the intensity values of the image data
+            transforms.NormalizeIntensityd(keys="image", nonzero=True, channel_wise=True),
             transforms.RandScaleIntensityd(keys="image", factors=0.1, prob=args.RandScaleIntensityd_prob),
-            # transform is used to randomly shift the intensity values of the image data
             transforms.RandShiftIntensityd(keys="image", offsets=0.1, prob=args.RandShiftIntensityd_prob),
-            # transform is used to convert the image and label data from numpy arrays to PyTorch tensors
             transforms.ToTensord(keys=["image", "label"]),
         ]
     )
     val_transform = transforms.Compose(
         [
             transforms.LoadImaged(keys=["image", "label"]),
-            transforms.AddChanneld(keys=["image", "label"]),
-            transforms.Orientationd(keys=["image", "label"], axcodes="RAS"),
-            transforms.Spacingd(
-                keys=["image", "label"], pixdim=(args.space_x, args.space_y, args.space_z), mode=("bilinear", "nearest")
-            ),
-            transforms.ScaleIntensityRanged(
-                keys=["image"], a_min=args.a_min, a_max=args.a_max, b_min=args.b_min, b_max=args.b_max, clip=True
-            ),
-            transforms.CropForegroundd(keys=["image", "label"], source_key="image"),
-            # todo can remove later
-            # Randomly crops a spatial region of size.
-            transforms.RandCropByPosNegLabeld(
-                keys=["image", "label"],
-                spatial_size=(args.roi_x, args.roi_y, args.roi_z),
-                pos=1,
-                neg=1,
-                num_samples=20,
-                image_key="image",
-                label_key="label",
-                image_threshold=0,
-            ),
+            transforms.ConvertToMultiChannelBasedOnBratsClassesd(keys="label"),
+            AsDiscreted(keys=["label"], threshold_values=True, logit_thresh=0.5, n_classes=args.out_channels),
+            transforms.NormalizeIntensityd(keys="image", nonzero=True, channel_wise=True),
             transforms.ToTensord(keys=["image", "label"]),
         ]
     )
-    if args.valid_loader != 'none':
+
+    if args.valid_loader == 'none':
         val_transform = transforms.Compose(
             [
                 transforms.LoadImaged(keys=["image", "label"]),
-                transforms.AddChanneld(keys=["image", "label"]),
-                transforms.Orientationd(keys=["image", "label"], axcodes="RAS"),
-                transforms.Spacingd(
-                    keys=["image", "label"], pixdim=(args.space_x, args.space_y, args.space_z),
-                    mode=("bilinear", "nearest")
+                transforms.ConvertToMultiChannelBasedOnBratsClassesd(keys="label"),
+                AsDiscreted(keys=["label"], threshold_values=True, logit_thresh=0.5, n_classes=args.out_channels),
+                transforms.NormalizeIntensityd(keys="image", nonzero=True, channel_wise=True),
+                transforms.RandCropByPosNegLabeld(
+                    keys=["image", "label"],
+                    spatial_size=(args.roi_x, args.roi_y, args.roi_z),
+                    pos=1,
+                    neg=1,
+                    num_samples=4,
+                    image_key="image",
+                    label_key="label",
+                    image_threshold=0,
                 ),
-                transforms.ScaleIntensityRanged(
-                    keys=["image"], a_min=args.a_min, a_max=args.a_max, b_min=args.b_min, b_max=args.b_max, clip=True
-                ),
-                transforms.CropForegroundd(keys=["image", "label"], source_key="image"),
                 transforms.ToTensord(keys=["image", "label"]),
             ]
         )
 
+    test_transform = transforms.Compose(
+        [
+            transforms.LoadImaged(keys=["image", "label"]),
+            transforms.ConvertToMultiChannelBasedOnBratsClassesd(keys="label"),
+            AsDiscreted(keys=["label"], threshold_values=True, logit_thresh=0.5, n_classes=args.out_channels),
+            transforms.NormalizeIntensityd(keys="image", nonzero=True, channel_wise=True),
+            transforms.ToTensord(keys=["image", "label"]),
+        ]
+    )
 
     if args.test_mode:
-        test_files = load_decathlon_datalist(datalist_json, True, "validation", base_dir=data_dir)
-        test_ds = data.Dataset(data=test_files, transform=val_transform)
-        test_sampler = Sampler(test_ds, shuffle=False) if args.distributed else None
+
+        val_ds = data.Dataset(data=validation_files, transform=test_transform)
+        val_sampler = Sampler(val_ds, shuffle=False) if args.distributed else None
         test_loader = data.DataLoader(
-            test_ds,
+            val_ds,
             batch_size=1,
             shuffle=False,
             num_workers=args.workers,
-            sampler=test_sampler,
-            pin_memory=True,
-            persistent_workers=True,
+            sampler=val_sampler,
+            pin_memory=True
         )
+
         loader = test_loader
     else:
-        datalist = load_decathlon_datalist(datalist_json, True, "training", base_dir=data_dir)
-        if args.use_normal_dataset:
-            train_ds = data.Dataset(data=datalist, transform=train_transform)
-        else:
-            train_ds = data.CacheDataset(
-                data=datalist, transform=train_transform, cache_num=24, cache_rate=1.0, num_workers=args.workers
-            )
+        train_ds = data.Dataset(data=train_files, transform=train_transform)
+
         train_sampler = Sampler(train_ds) if args.distributed else None
         train_loader = data.DataLoader(
             train_ds,
@@ -182,8 +178,7 @@ def get_loader(args):
             pin_memory=True,
             persistent_workers=True,
         )
-        val_files = load_decathlon_datalist(datalist_json, True, "validation", base_dir=data_dir)
-        val_ds = data.Dataset(data=val_files, transform=val_transform)
+        val_ds = data.Dataset(data=validation_files, transform=val_transform)
         val_sampler = Sampler(val_ds, shuffle=False) if args.distributed else None
         val_loader = data.DataLoader(
             val_ds,
@@ -191,35 +186,8 @@ def get_loader(args):
             shuffle=False,
             num_workers=args.workers,
             sampler=val_sampler,
-            pin_memory=True,
-            persistent_workers=True,
+            pin_memory=True
         )
         loader = [train_loader, val_loader]
 
     return loader
-#
-#
-# class Config:
-#     def __init__(self):
-#         self.base_data = '/media/amin/SP PHD U3/CT_Segmentation_Images/3D/MSD/Abdomen/RawData/Training'
-#         self.json_list = '../../input_list/dataset_MSD_List.json'
-#         self.space_x = 1.5
-#         self.space_y = 1.5
-#         self.space_z = 2.0
-#         self.a_min = -175.0
-#         self.a_max = 250.0
-#         self.b_min = 0.0
-#         self.b_max = 1.0
-#         self.roi_x = 96
-#         self.roi_y = 96
-#         self.roi_z = 96
-#         self.RandFlipd_prob = 0.2
-#         self.RandRotate90d_prob = 0.2
-#         self.RandScaleIntensityd_prob = 0.1
-#         self.RandShiftIntensityd_prob = 0.1
-#         self.test_mode = False
-#         self.workers = 0
-#         self.distributed = False
-#
-#
-# a = get_loader(args=Config())
