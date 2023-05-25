be964871cd3ba70c5d149fc823579cd7595466f6
amin.karimi <amin.karimi1992@gmail.com>
2023-05-25 11:15:01 -0400

    change dataloader

--- BRATS21/main.py
+++ BRATS21/main.py
@@ -45,8 +45,8 @@ parser.add_argument("--base_data",
                     type=str,
                     help="dataset directory")
 parser.add_argument("--a_min", default=-100.0, type=float, help="a_min in ScaleIntensityRanged")
-parser.add_argument("--a_max", default=2000.0, type=float, help="a_max in ScaleIntensityRanged")
-parser.add_argument("--b_min", default=-1.0, type=float, help="b_min in ScaleIntensityRanged")
+parser.add_argument("--a_max", default=200.0, type=float, help="a_max in ScaleIntensityRanged")
+parser.add_argument("--b_min", default=0.0, type=float, help="b_min in ScaleIntensityRanged")
 parser.add_argument("--b_max", default=1.0, type=float, help="b_max in ScaleIntensityRanged")
 parser.add_argument("--sw_batch_size", default=4, type=int, help="number of sliding window batch size")
 parser.add_argument("--space_x", default=1.0, type=float, help="spacing in x direction")
