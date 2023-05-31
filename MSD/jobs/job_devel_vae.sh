5d17ed854b3bc199ba2cda3fa350233c95cb09e7
amin.karimi <amin.karimi1992@gmail.com>
2023-05-31 11:18:11 -0400

    fix main and job of MSD

--- MSD/jobs/job_devel_vae.sh
+++ MSD/jobs/job_devel_vae.sh
@@ -293,7 +293,7 @@ then
   PYTHONPATH=. python3 MSD/main.py  --workers 8 --base_data ../images/MSD \
   --json_list input_list/dataset_MSD_List.json --save_checkpoint --max_epochs 15000 \
   --distributed --use_normal_dataset --batch_size 1 --num_stages 4 --embed_dims 96 192 384 768 --depths 6 6 90 6 \
-  --mlp_ratios 8 8 4 4 --roi_x 128 --roi_y 128 --roi_z 128 --val_every 1 --model_inferer inferer \
+  --mlp_ratios 8 8 4 4 --roi_x 96 --roi_y 96 --roi_z 96 --val_every 1 --model_inferer inferer \
   --valid_loader valid_loader --model_v VANV4 --upsample vae --checkpoint --task $task 
 
 elif [ $job -eq 49 ]
@@ -367,7 +367,7 @@ then
   PYTHONPATH=. python3 MSD/main.py  --workers 8 --base_data ../images/MSD \
   --json_list input_list/dataset_MSD_List.json --save_checkpoint --max_epochs 15000 \
   --distributed --use_normal_dataset --batch_size 1 --num_stages 4 --embed_dims 96 192 384 768 --depths 6 6 90 6 \
-  --mlp_ratios 8 8 4 4 --roi_x 128 --roi_y 128 --roi_z 128 --val_every 1 --model_inferer inferer \
+  --mlp_ratios 8 8 4 4 --roi_x 96 --roi_y 96 --roi_z 96 --val_every 1 --model_inferer inferer \
   --valid_loader valid_loader --model_v VANV4GL --upsample vae --checkpoint --task $task  --patch_count 2
 
 elif [ $job -eq 429 ]
@@ -626,7 +626,7 @@ then
   PYTHONPATH=. python3 MSD/main.py  --workers 8 --base_data ../images/MSD \
   --json_list input_list/dataset_MSD_List.json --save_checkpoint --max_epochs 15000 \
   --distributed --use_normal_dataset --batch_size 1 --num_stages 4 --embed_dims 96 192 384 768 --depths 6 6 90 6 \
-  --mlp_ratios 8 8 4 4 --roi_x 128 --roi_y 128 --roi_z 128 --val_every 1 --model_inferer inferer \
+  --mlp_ratios 8 8 4 4 --roi_x 96 --roi_y 96 --roi_z 96 --val_every 1 --model_inferer inferer \
   --valid_loader valid_loader --model_v VANV5GL --upsample vae --checkpoint --task $task  --patch_count 2
 
 elif [ $job -eq 529 ]
