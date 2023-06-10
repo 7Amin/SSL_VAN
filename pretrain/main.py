a131a8065d8be7a50ab560b4b1519d3143a00bcd
amin <amin.karimi1992@gmail.com>
2023-06-10 14:31:18 -0400

    fix main

--- pretrain/main.py
+++ pretrain/main.py
@@ -160,26 +160,28 @@ def main_worker(gpu, args):
     scheduler = get_lr_schedule(args, optimizer, start_epoch)
 
     if args.checkpoint is not None and args.checkpoint:
-        checkpoint = torch.load(os.path.join(args.logdir, args.final_model_url), map_location="cpu")
-        from collections import OrderedDict
+        temp = os.path.join(args.logdir, args.final_model_url)
+        if os.path.isfile(temp):
+            checkpoint = torch.load(temp, map_location="cpu")
+            from collections import OrderedDict
 
-        new_state_dict = OrderedDict()
-        for k, v in checkpoint["state_dict"].items():
-            new_state_dict[k.replace("backbone.", "")] = v
-        model.load_state_dict(new_state_dict, strict=False)
-        if "epoch" in checkpoint:
-            start_epoch = checkpoint["epoch"]
-        if "best_acc" in checkpoint:
-            best_acc = checkpoint["best_acc"]
-        if 'optimizer' in checkpoint:
-            optimizer = get_optimizer(model, args)
-        #     optimizer_temp = checkpoint['optimizer']
-        #     optimizer.load_state_dict(optimizer_temp)
-        if 'scheduler' in checkpoint:
-            scheduler_temp = checkpoint['scheduler']
-            scheduler.load_state_dict(scheduler_temp)
-        warnings.warn("=> loaded checkpoint '{}' (epoch {}) (bestacc {})".format(
-            args.checkpoint, start_epoch, best_acc))
+            new_state_dict = OrderedDict()
+            for k, v in checkpoint["state_dict"].items():
+                new_state_dict[k.replace("backbone.", "")] = v
+            model.load_state_dict(new_state_dict, strict=False)
+            if "epoch" in checkpoint:
+                start_epoch = checkpoint["epoch"]
+            if "best_acc" in checkpoint:
+                best_acc = checkpoint["best_acc"]
+            if 'optimizer' in checkpoint:
+                optimizer = get_optimizer(model, args)
+                # optimizer_temp = checkpoint['optimizer']
+                # optimizer.load_state_dict(optimizer_temp)
+            if 'scheduler' in checkpoint:
+                scheduler_temp = checkpoint['scheduler']
+                scheduler.load_state_dict(scheduler_temp)
+            warnings.warn("=> loaded checkpoint '{}' (epoch {}) (bestacc {})".format(
+                args.checkpoint, start_epoch, best_acc))
 
     model.cuda(args.gpu)
 
