2ac6fa06214a83cc300f655ab0faf253cde3e09e
amin <amin.karimi1992@gmail.com>
2023-06-10 14:43:00 -0400

    fix util

--- pretrain/utils/utils.py
+++ pretrain/utils/utils.py
@@ -74,7 +74,11 @@ def load_clusters(json_path='./cluster_paths.json'):
     res = []
     f = open(json_path)
     models_path = json.load(f)
+    print(json_path)
     for model_path in models_path:
-        loaded_model = joblib.load(model_path)
+        loaded_model = joblib.load(model_path['url'])
         res.append(loaded_model)
     return res
+
+
+# load_clusters('./../../cluster_paths.json')
