2c58b4d5254730d0e95ed6306c9b9b9bf3a6136b
amin <amin.karimi1992@gmail.com>
2023-06-10 00:13:52 -0400

    fix util bug

--- pretrain/utils/data_utils.py
+++ pretrain/utils/data_utils.py
@@ -116,7 +116,7 @@ def get_loader(args):
     else:
         train_sampler = None
     train_loader = DataLoader(
-        train_ds, batch_size=args.batch_size, num_workers=args.num_workers, sampler=train_sampler, drop_last=True
+        train_ds, batch_size=args.batch_size, num_workers=args.workers, sampler=train_sampler, drop_last=True
         , shuffle=True
     )
     print("loader is ready")
