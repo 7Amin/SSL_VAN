2b7730e8203eb4922f51f7c1e9d80cf22b632687
amin <amin.karimi1992@gmail.com>
2023-06-22 15:28:35 -0400

    remove log

--- commons/util.py
+++ commons/util.py
@@ -4,10 +4,10 @@ import warnings
 
 def fix_outputs_url(args, base_url):
     output_url = "output_" + str(args.test_mode) + "_" + str(args.val_mode) + "/"
-    if not os.path.isdir(output_url):
+    if not os.path.exists(output_url):
         os.mkdir(output_url)
     output_url = output_url + base_url
-    if not os.path.isdir(output_url):
+    if not os.path.exists(output_url):
         os.mkdir(output_url)
     args.output_url = output_url
     args.best_model_url = base_url + "_" + "_best.pt"
