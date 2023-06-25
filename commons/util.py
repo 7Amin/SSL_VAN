b7de4ea735b13228d65f24e9ff1642be5fbb887b
amin <amin.karimi1992@gmail.com>
2023-06-25 13:04:28 -0400

    fix output for image

--- commons/util.py
+++ commons/util.py
@@ -5,11 +5,13 @@ import warnings
 def fix_outputs_url(args, base_url):
     warnings.warn(f" base_url is {base_url}")
     output_url = os.path.join(args.logdir, "output_" + str(args.test_mode) + "_" + str(args.val_mode))
-    warnings.warn(f" output_url is {output_url}")
+    warnings.warn(f" output_url 1 is {output_url}")
+    warnings.warn(f" exists 1 is {os.path.exists(output_url)}")
     if not os.path.exists(output_url):
         os.mkdir(output_url)
     output_url = output_url + base_url
-    warnings.warn(f" output_url is {output_url}")
+    warnings.warn(f" output_url 2 is {output_url}")
+    warnings.warn(f" exists 2 is {os.path.exists(output_url)}")
     if not os.path.exists(output_url):
         os.mkdir(output_url)
     args.output_url = output_url
