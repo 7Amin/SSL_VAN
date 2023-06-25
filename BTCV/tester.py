9010e5ae3f008437321814f39b60d933e941524c
amin <amin.karimi1992@gmail.com>
2023-06-25 12:04:30 -0400

    add test for BTCV

--- BTCV/tester.py
+++ BTCV/tester.py
@@ -56,8 +56,8 @@ def test_eval(model, loader, acc_func, args, model_inferer=None, post_label=None
                 for al, nl in zip(acc_list):
                     run_acc.update(al)
             else:
-                run_acc.update(acc.cpu().numpy()[0])
-            warnings.warn("acc {}".format(acc.cpu().numpy()[0]))
+                run_acc.update(np.nan_to_num(acc.cpu().numpy()[0], nan=100.0))
+            warnings.warn("acc {}".format(np.nan_to_num(acc.cpu().numpy()[0], nan=100.0)))
 
             if args.rank == 0:
                 Dice_background = run_acc.avg[0]
