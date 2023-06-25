9d79ae884813b00c8997ff261a05f5f90a0ca50d
amin <amin.karimi1992@gmail.com>
2023-06-25 13:18:57 -0400

    fix output for image

--- commons/util.py
+++ commons/util.py
@@ -7,13 +7,13 @@ def fix_outputs_url(args, base_url):
     output_url = os.path.join(args.logdir, "output_" + str(args.test_mode) + "_" + str(args.val_mode))
     warnings.warn(f" output_url 1 is {output_url}")
     warnings.warn(f" exists 1 is {os.path.exists(output_url)}")
-    # if not os.path.isdir(output_url):
-    #     os.mkdir(output_url)
+    if not os.path.isdir(output_url):
+        os.mkdir(output_url)
     output_url = output_url + "/" + base_url
     warnings.warn(f" output_url 2 is {output_url}")
     warnings.warn(f" exists 2 is {os.path.exists(output_url)}")
-    # if not os.path.exists(output_url):
-    #     os.mkdir(output_url)
+    if not os.path.exists(output_url):
+        os.mkdir(output_url)
     args.output_url = output_url
     args.best_model_url = base_url + "_" + "_best.pt"
     args.final_model_url = base_url + "_" + "_final.pt"
