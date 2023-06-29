1f9741969ac90063d0326756ff9c92ee19765497
amin <amin.karimi1992@gmail.com>
2023-06-28 22:49:07 -0400

    fix main

--- BRATS21/main.py
+++ BRATS21/main.py
@@ -93,8 +93,11 @@ parser.add_argument("--upsample", default="vae", type=str, choices=['deconv', 'v
 parser.add_argument("--model_inferer", default='inferer', type=str, choices=['none', 'inferer'])
 parser.add_argument("--valid_loader", default='valid_loader', type=str, choices=['none', 'valid_loader'])
 parser.add_argument("--model_v", default='VANV5GL', type=str, choices=['VAN', 'VANV2', 'VANV3', 'VANV4', 'VANV4GL',
-                                                                       'VANV4GLV1', 'VANV4GLV2', 'VANV5', 'VANV5GL',
-                                                                       "VANV6GL"])
+                                                                       'VANV4GLV1', 'VANV4GLV2', 'VANV5GL', "VANV6GL",
+                                                                       'SwinUNETR24', 'SwinUNETR36', 'SwinUNETR48',
+                                                                       'UNETR16', 'UNETR32', 'nnUNet', 'SegResNetVAE',
+                                                                       'Unetpp', 'AttentionUnet', 'BasicUNetPlusPlus',
+                                                                       'DiNTS_Search', 'DiNTS_Instance'])
 parser.add_argument("--patch_count", default=2, type=int, help="split image to patches")
 parser.add_argument("--clip", default=1000.0, type=int, help="Clips gradient norm of an iterable of parameters.")
 
