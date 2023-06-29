a95ba41bd9f94fdcbcb93d13024d9121afa94ab6
amin <amin.karimi1992@gmail.com>
2023-06-29 08:48:07 -0400

    fix Bra jobs

--- BRATS21/jobs/job_base.sh
+++ BRATS21/jobs/job_base.sh
@@ -20,7 +20,7 @@ then
 
     PYTHONPATH=. python3 BRATS21/main.py  --workers 8 --base_data ../images/BraTS21 \
   --json_list input_list/dataset_BRATS21_List.json --logdir ./runs/BraTS21_new/test_log --save_checkpoint --max_epochs 1000 \
-  --distributed --use_normal_dataset --batch_size 1 -roi_x 128 --roi_y 128 --roi_z 128 --val_every 10 --model_inferer inferer \
+  --distributed --use_normal_dataset --batch_size 1 --roi_x 128 --roi_y 128 --roi_z 128 --val_every 10 --model_inferer inferer \
   --valid_loader valid_loader --model_v SwinUNETR24 --checkpoint
 
 elif [ $job -eq 736 ]
