044d64de3e2330a5592129d65c50a9ba58979674
amin <amin.karimi1992@gmail.com>
2023-07-04 08:12:17 -0400

    remove log

--- BTCV/jobs/job_main_vae_pre_new.sh
+++ BTCV/jobs/job_main_vae_pre_new.sh
@@ -75,7 +75,7 @@ elif [ $job -eq 41227 ]
 then
   PYTHONPATH=. python3 BTCV/main.py --use_ssl_pretrained --workers 8 --base_data ../images/BTCV/Abdomen/RawData/Training \
   --json_list input_list/dataset_BTCV_List.json  --logdir ./runs/BTCV_new/test_log --save_checkpoint --max_epochs 4000 \
-  --distributed --use_normal_dataset --batch_size 2 --num_stages 4 --embed_dims 64 128 256 512 --depths 3 4 6 3 \
+  --distributed --use_normal_dataset --batch_size 1 --num_stages 4 --embed_dims 64 128 256 512 --depths 3 4 6 3 \
   --mlp_ratios 8 8 4 4 --roi_x 96 --roi_y 96 --roi_z 96 --val_every 5 --model_inferer inferer \
   --valid_loader valid_loader --model_v VANV4122GL --upsample vae --checkpoint \
   --pretrained_model_name pre_train_64-128-256-512_3-4-6-3_8-8-4-4_vae_inferer_valid_loader_PREVANV4121GL_2_1_500_128_True_5__final.pt
@@ -84,7 +84,7 @@ elif [ $job -eq 41229 ]
 then
   PYTHONPATH=. python3 BTCV/main.py --use_ssl_pretrained --workers 8 --base_data ../images/BTCV/Abdomen/RawData/Training \
   --json_list input_list/dataset_BTCV_List.json  --logdir ./runs/BTCV_new/test_log --save_checkpoint --max_epochs 4000 \
-  --distributed --use_normal_dataset --batch_size 2 --num_stages 4 --embed_dims 96 192 384 768 --depths 3 3 24 3 \
+  --distributed --use_normal_dataset --batch_size 1 --num_stages 4 --embed_dims 96 192 384 768 --depths 3 3 24 3 \
   --mlp_ratios 8 8 4 4 --roi_x 96 --roi_y 96 --roi_z 96 --val_every 5 --model_inferer inferer \
   --valid_loader valid_loader --model_v VANV4122GL --upsample vae --checkpoint \
   --pretrained_model_name pre_train_96-192-384-768_3-3-24-3_8-8-4-4_vae_inferer_valid_loader_PREVANV4121GL_2_1_500_128_True_5__final.pt
