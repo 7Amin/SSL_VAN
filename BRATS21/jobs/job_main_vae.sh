331cce9e7192826216c0feda05e6e19e5c13bf5c
amin <amin.karimi1992@gmail.com>
2023-07-10 07:47:43 -0400

    remove log

--- BRATS21/jobs/job_main_vae.sh
+++ BRATS21/jobs/job_main_vae.sh
@@ -309,7 +309,7 @@ then
   PYTHONPATH=. python3 BRATS21/main.py  --workers 8 --optim_lr 0.0001 --base_data ../images/BraTS21 \
   --json_list input_list/dataset_BRATS21_List.json  --save_checkpoint --max_epochs 250 \
   --distributed --use_normal_dataset --batch_size 1 --num_stages 4 --embed_dims 64 128 256 512 --depths 3 4 6 3 \
-  --mlp_ratios 8 8 4 4 --roi_x 128 --roi_y 128 --roi_z 128 --val_every 10 --model_inferer inferer \
+  --mlp_ratios 8 8 4 4 --roi_x 128 --roi_y 128 --roi_z 128 --val_every 1 --model_inferer inferer \
   --valid_loader valid_loader --model_v VANV412 --upsample vae --checkpoint
 
 elif [ $job -eq 4129 ]
@@ -317,7 +317,7 @@ then
   PYTHONPATH=. python3 BRATS21/main.py  --workers 8 --optim_lr 0.0001 --base_data ../images/BraTS21 \
   --json_list input_list/dataset_BRATS21_List.json  --save_checkpoint --max_epochs 250 \
   --distributed --use_normal_dataset --batch_size 1 --num_stages 4 --embed_dims 96 192 384 768 --depths 3 3 24 3 \
-  --mlp_ratios 8 8 4 4 --roi_x 128 --roi_y 128 --roi_z 128 --val_every 10 --model_inferer inferer \
+  --mlp_ratios 8 8 4 4 --roi_x 128 --roi_y 128 --roi_z 128 --val_every 1 --model_inferer inferer \
   --valid_loader valid_loader --model_v VANV412 --upsample vae --checkpoint
 
 elif [ $job -eq 41217 ]
@@ -325,7 +325,7 @@ then
   PYTHONPATH=. python3 BRATS21/main.py  --workers 8 --optim_lr 0.0001 --base_data ../images/BraTS21 \
   --json_list input_list/dataset_BRATS21_List.json  --save_checkpoint --max_epochs 250 \
   --distributed --use_normal_dataset --batch_size 1 --num_stages 4 --embed_dims 64 128 256 512 --depths 3 4 6 3 \
-  --mlp_ratios 8 8 4 4 --roi_x 128 --roi_y 128 --roi_z 128 --val_every 10 --model_inferer inferer \
+  --mlp_ratios 8 8 4 4 --roi_x 128 --roi_y 128 --roi_z 128 --val_every 1 --model_inferer inferer \
   --valid_loader valid_loader --model_v VANV4121GL --upsample vae --checkpoint
 
 elif [ $job -eq 41219 ]
@@ -333,7 +333,7 @@ then
   PYTHONPATH=. python3 BRATS21/main.py  --workers 8 --optim_lr 0.0001 --base_data ../images/BraTS21 \
   --json_list input_list/dataset_BRATS21_List.json  --save_checkpoint --max_epochs 250 \
   --distributed --use_normal_dataset --batch_size 1 --num_stages 4 --embed_dims 96 192 384 768 --depths 3 3 24 3 \
-  --mlp_ratios 8 8 4 4 --roi_x 128 --roi_y 128 --roi_z 128 --val_every 10 --model_inferer inferer \
+  --mlp_ratios 8 8 4 4 --roi_x 128 --roi_y 128 --roi_z 128 --val_every 1 --model_inferer inferer \
   --valid_loader valid_loader --model_v VANV4121GL --upsample vae --checkpoint
 
 elif [ $job -eq 41227 ]
@@ -341,7 +341,7 @@ then
   PYTHONPATH=. python3 BRATS21/main.py  --workers 8 --optim_lr 0.0001 --base_data ../images/BraTS21 \
   --json_list input_list/dataset_BRATS21_List.json  --save_checkpoint --max_epochs 250 \
   --distributed --use_normal_dataset --batch_size 1 --num_stages 4 --embed_dims 64 128 256 512 --depths 3 4 6 3 \
-  --mlp_ratios 8 8 4 4 --roi_x 128 --roi_y 128 --roi_z 128 --val_every 10 --model_inferer inferer \
+  --mlp_ratios 8 8 4 4 --roi_x 128 --roi_y 128 --roi_z 128 --val_every 1 --model_inferer inferer \
   --valid_loader valid_loader --model_v VANV4122GL --upsample vae --checkpoint
 
 elif [ $job -eq 41229 ]
@@ -349,7 +349,7 @@ then
   PYTHONPATH=. python3 BRATS21/main.py  --workers 8 --optim_lr 0.0001 --base_data ../images/BraTS21 \
   --json_list input_list/dataset_BRATS21_List.json  --save_checkpoint --max_epochs 250 \
   --distributed --use_normal_dataset --batch_size 1 --num_stages 4 --embed_dims 96 192 384 768 --depths 3 3 24 3 \
-  --mlp_ratios 8 8 4 4 --roi_x 128 --roi_y 128 --roi_z 128 --val_every 10 --model_inferer inferer \
+  --mlp_ratios 8 8 4 4 --roi_x 128 --roi_y 128 --roi_z 128 --val_every 1 --model_inferer inferer \
   --valid_loader valid_loader --model_v VANV4122GL --upsample vae --checkpoint
 
 
