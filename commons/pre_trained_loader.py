7e92f45c4b070505a26734a05ccf4811ddb7d527
amin <amin.karimi1992@gmail.com>
2023-07-12 11:08:41 -0400

    remove log

--- commons/pre_trained_loader.py
+++ commons/pre_trained_loader.py
@@ -8,12 +8,15 @@ def load_pre_trained(args, model):
     model_dict = torch.load(model_url)
     new_state_dict = OrderedDict()
     state_dict = model_dict["state_dict"]
-
-    if "VANV4" in args.model_v or "VANV4GL" in args.model_v or "VANV5GL" in args.model_v or "VANV6GL" in args.model_v:
-        for key in list(state_dict.keys()):
-            if not ("pre_train_proj" in key):
-                new_state_dict[key] = state_dict.pop(key)
-        model.load_state_dict(new_state_dict, strict=False)
-        warnings.warn(f"{args.model_v} - Using pretrained self-supervised backbone weights !")
+    warnings.warn(f"new_state_dict len is {new_state_dict} - before")
+    # if "VANV4" in args.model_v or "VANV4GL" in args.model_v or "VANV5GL" in args.model_v or "VANV6GL" in args.model_v:
+    for key in list(state_dict.keys()):
+        if not ("pre_train_proj" in key):
+            new_state_dict[key] = state_dict.pop(key)
+    warnings.warn(f"new_state_dict len is {new_state_dict} - after")
+    loaded_keys = model.load_state_dict(new_state_dict, strict=False)
+    warnings.warn(f"{args.model_v} - Using pretrained self-supervised backbone weights !")
+    matched_keys = sum(k in model.state_dict() for k in loaded_keys.keys())
+    print(f"Number of matched keys loaded: {matched_keys}")
     args.model_v = args.model_v + "_pre"
     return model
