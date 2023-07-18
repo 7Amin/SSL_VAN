b05fb7b34b8a01ca86276f493fa65445d5d5c607
amin <amin.karimi1992@gmail.com>
2023-07-18 17:51:31 -0400

    remove log

--- BTCV/jobs/job_main_vae_pre_new.sh
+++ BTCV/jobs/job_main_vae_pre_new.sh
@@ -19,7 +19,7 @@ cluster_size=$2
 freeze=$3
 
 
-if [ $job -eq 4127 ] && [ $cluster_size -eq 1 ] && [ $freeze -eq 1]
+if [ $job -eq 4127 ] && [ $cluster_size -eq 1 ] && [ $freeze -eq 1 ]
 then
   PYTHONPATH=. python3 BTCV/main.py --use_ssl_pretrained --workers 8 --base_data ../images/BTCV/Abdomen/RawData/Training \
   --json_list input_list/dataset_BTCV_List.json  --logdir ./runs/BTCV_new/test_log1 --save_checkpoint --max_epochs 5000 \
@@ -28,7 +28,7 @@ then
   --valid_loader valid_loader --model_v VANV412 --upsample vae --lrschedule none --optim_lr 0.0002 --checkpoint \
   --pretrained_model_name pre_train_64-128-256-512_3-4-6-3_8-8-4-4_vae_inferer_valid_loader_PREVANV412_1_500_128_True_5_pre_version2__final.pt
 
-elif [ $job -eq 4127 ] && [ $cluster_size -eq 10 ] && [ $freeze -eq 1]
+elif [ $job -eq 4127 ] && [ $cluster_size -eq 10 ] && [ $freeze -eq 1 ]
 then
   PYTHONPATH=. python3 BTCV/main.py --use_ssl_pretrained --workers 8 --base_data ../images/BTCV/Abdomen/RawData/Training \
   --json_list input_list/dataset_BTCV_List.json  --logdir ./runs/BTCV_new/test_log10 --save_checkpoint --max_epochs 5000 \
@@ -37,7 +37,7 @@ then
   --valid_loader valid_loader --model_v VANV412 --upsample vae --lrschedule none --optim_lr 0.0002 --checkpoint \
   --pretrained_model_name pre_train_64-128-256-512_3-4-6-3_8-8-4-4_vae_inferer_valid_loader_PREVANV412_10_500_128_True_5_pre_version2__final.pt
 
-elif [ $job -eq 4127 ] && [ $cluster_size -eq 20 ] && [ $freeze -eq 1]
+elif [ $job -eq 4127 ] && [ $cluster_size -eq 20 ] && [ $freeze -eq 1 ]
 then
   PYTHONPATH=. python3 BTCV/main.py --use_ssl_pretrained --workers 8 --base_data ../images/BTCV/Abdomen/RawData/Training \
   --json_list input_list/dataset_BTCV_List.json  --logdir ./runs/BTCV_new/test_log20 --save_checkpoint --max_epochs 5000 \
@@ -46,7 +46,7 @@ then
   --valid_loader valid_loader --model_v VANV412 --upsample vae --lrschedule none --optim_lr 0.0002 --checkpoint \
   --pretrained_model_name pre_train_64-128-256-512_3-4-6-3_8-8-4-4_vae_inferer_valid_loader_PREVANV412_20_500_128_True_5_pre_version2__final.pt
 
-elif [ $job -eq 4127 ] && [ $cluster_size -eq 40 ] && [ $freeze -eq 1]
+elif [ $job -eq 4127 ] && [ $cluster_size -eq 40 ] && [ $freeze -eq 1 ]
 then
   PYTHONPATH=. python3 BTCV/main.py --use_ssl_pretrained --workers 8 --base_data ../images/BTCV/Abdomen/RawData/Training \
   --json_list input_list/dataset_BTCV_List.json  --logdir ./runs/BTCV_new/test_log40 --save_checkpoint --max_epochs 5000 \
@@ -55,7 +55,7 @@ then
   --valid_loader valid_loader --model_v VANV412 --upsample vae --lrschedule none --optim_lr 0.0002 --checkpoint \
   --pretrained_model_name pre_train_64-128-256-512_3-4-6-3_8-8-4-4_vae_inferer_valid_loader_PREVANV412_40_500_128_True_5_pre_version2__final.pt
 
-elif [ $job -eq 4127 ] && [ $cluster_size -eq 80 ] && [ $freeze -eq 1]
+elif [ $job -eq 4127 ] && [ $cluster_size -eq 80 ] && [ $freeze -eq 1 ]
 then
   PYTHONPATH=. python3 BTCV/main.py --use_ssl_pretrained --workers 8 --base_data ../images/BTCV/Abdomen/RawData/Training \
   --json_list input_list/dataset_BTCV_List.json  --logdir ./runs/BTCV_new/test_log80 --save_checkpoint --max_epochs 5000 \
@@ -64,7 +64,7 @@ then
   --valid_loader valid_loader --model_v VANV412 --upsample vae --lrschedule none --optim_lr 0.0002 --checkpoint \
   --pretrained_model_name pre_train_64-128-256-512_3-4-6-3_8-8-4-4_vae_inferer_valid_loader_PREVANV412_80_500_128_True_5_pre_version2__final.pt
 
-elif [ $job -eq 4129 ] && [ $cluster_size -eq 1 ] && [ $freeze -eq 1]
+elif [ $job -eq 4129 ] && [ $cluster_size -eq 1 ] && [ $freeze -eq 1 ]
 then
   PYTHONPATH=. python3 BTCV/main.py --use_ssl_pretrained --workers 8 --base_data ../images/BTCV/Abdomen/RawData/Training \
   --json_list input_list/dataset_BTCV_List.json  --logdir ./runs/BTCV_new/test_log1 --save_checkpoint --max_epochs 5000 \
@@ -73,7 +73,7 @@ then
   --valid_loader valid_loader --model_v VANV412 --upsample vae --lrschedule none --optim_lr 0.0002 --checkpoint \
   --pretrained_model_name pre_train_96-192-384-768_3-3-24-3_8-8-4-4_vae_inferer_valid_loader_PREVANV412_1_500_128_True_5_pre_version2__final.pt
 
-elif [ $job -eq 4129 ] && [ $cluster_size -eq 10 ] && [ $freeze -eq 1]
+elif [ $job -eq 4129 ] && [ $cluster_size -eq 10 ] && [ $freeze -eq 1 ]
 then
   PYTHONPATH=. python3 BTCV/main.py --use_ssl_pretrained --workers 8 --base_data ../images/BTCV/Abdomen/RawData/Training \
   --json_list input_list/dataset_BTCV_List.json  --logdir ./runs/BTCV_new/test_log10 --save_checkpoint --max_epochs 5000 \
@@ -82,7 +82,7 @@ then
   --valid_loader valid_loader --model_v VANV412 --upsample vae --lrschedule none --optim_lr 0.0002 --checkpoint \
   --pretrained_model_name pre_train_96-192-384-768_3-3-24-3_8-8-4-4_vae_inferer_valid_loader_PREVANV412_10_500_128_True_5_pre_version2__final.pt
 
-elif [ $job -eq 4129 ] && [ $cluster_size -eq 20 ] && [ $freeze -eq 1]
+elif [ $job -eq 4129 ] && [ $cluster_size -eq 20 ] && [ $freeze -eq 1 ]
 then
   PYTHONPATH=. python3 BTCV/main.py --use_ssl_pretrained --workers 8 --base_data ../images/BTCV/Abdomen/RawData/Training \
   --json_list input_list/dataset_BTCV_List.json  --logdir ./runs/BTCV_new/test_log20 --save_checkpoint --max_epochs 5000 \
@@ -91,7 +91,7 @@ then
   --valid_loader valid_loader --model_v VANV412 --upsample vae --lrschedule none --optim_lr 0.0002 --checkpoint \
   --pretrained_model_name pre_train_96-192-384-768_3-3-24-3_8-8-4-4_vae_inferer_valid_loader_PREVANV412_20_500_128_True_5_pre_version2__final.pt
 
-elif [ $job -eq 4129 ] && [ $cluster_size -eq 40 ] && [ $freeze -eq 1]
+elif [ $job -eq 4129 ] && [ $cluster_size -eq 40 ] && [ $freeze -eq 1 ]
 then
   PYTHONPATH=. python3 BTCV/main.py --use_ssl_pretrained --workers 8 --base_data ../images/BTCV/Abdomen/RawData/Training \
   --json_list input_list/dataset_BTCV_List.json  --logdir ./runs/BTCV_new/test_log40 --save_checkpoint --max_epochs 5000 \
@@ -100,7 +100,7 @@ then
   --valid_loader valid_loader --model_v VANV412 --upsample vae --lrschedule none --optim_lr 0.0002 --checkpoint \
   --pretrained_model_name pre_train_96-192-384-768_3-3-24-3_8-8-4-4_vae_inferer_valid_loader_PREVANV412_40_500_128_True_5_pre_version2__final.pt
 
-elif [ $job -eq 4129 ] && [ $cluster_size -eq 80 ] && [ $freeze -eq 1]
+elif [ $job -eq 4129 ] && [ $cluster_size -eq 80 ] && [ $freeze -eq 1 ]
 then
   PYTHONPATH=. python3 BTCV/main.py --use_ssl_pretrained --workers 8 --base_data ../images/BTCV/Abdomen/RawData/Training \
   --json_list input_list/dataset_BTCV_List.json  --logdir ./runs/BTCV_new/test_log80 --save_checkpoint --max_epochs 5000 \
@@ -109,7 +109,7 @@ then
   --valid_loader valid_loader --model_v VANV412 --upsample vae --lrschedule none --optim_lr 0.0002 --checkpoint \
   --pretrained_model_name pre_train_96-192-384-768_3-3-24-3_8-8-4-4_vae_inferer_valid_loader_PREVANV412_80_500_128_True_5_pre_version2__final.pt
 
-elif [ $job -eq 41227 ] && [ $cluster_size -eq 1 ] && [ $freeze -eq 1]
+elif [ $job -eq 41227 ] && [ $cluster_size -eq 1 ] && [ $freeze -eq 1 ]
 then
   PYTHONPATH=. python3 BTCV/main.py --use_ssl_pretrained --workers 8 --base_data ../images/BTCV/Abdomen/RawData/Training \
   --json_list input_list/dataset_BTCV_List.json  --logdir ./runs/BTCV_new/test_log1 --save_checkpoint --max_epochs 5000 \
@@ -118,7 +118,7 @@ then
   --valid_loader valid_loader --model_v VANV4122GL --upsample vae --lrschedule none --optim_lr 0.0002 --checkpoint \
   --pretrained_model_name pre_train_64-128-256-512_3-4-6-3_8-8-4-4_vae_inferer_valid_loader_PREVANV4121GL_2_1_500_128_True_5_pre_version2__final.pt
 
-elif [ $job -eq 41227 ] && [ $cluster_size -eq 10 ] && [ $freeze -eq 1]
+elif [ $job -eq 41227 ] && [ $cluster_size -eq 10 ] && [ $freeze -eq 1 ]
 then
   PYTHONPATH=. python3 BTCV/main.py --use_ssl_pretrained --workers 8 --base_data ../images/BTCV/Abdomen/RawData/Training \
   --json_list input_list/dataset_BTCV_List.json  --logdir ./runs/BTCV_new/test_log10 --save_checkpoint --max_epochs 5000 \
@@ -127,7 +127,7 @@ then
   --valid_loader valid_loader --model_v VANV4122GL --upsample vae --lrschedule none --optim_lr 0.0002 --checkpoint \
   --pretrained_model_name pre_train_64-128-256-512_3-4-6-3_8-8-4-4_vae_inferer_valid_loader_PREVANV4121GL_2_10_500_128_True_5_pre_version2__final.pt
 
-elif [ $job -eq 41227 ] && [ $cluster_size -eq 20 ] && [ $freeze -eq 1]
+elif [ $job -eq 41227 ] && [ $cluster_size -eq 20 ] && [ $freeze -eq 1 ]
 then
   PYTHONPATH=. python3 BTCV/main.py --use_ssl_pretrained --workers 8 --base_data ../images/BTCV/Abdomen/RawData/Training \
   --json_list input_list/dataset_BTCV_List.json  --logdir ./runs/BTCV_new/test_log20 --save_checkpoint --max_epochs 5000 \
@@ -136,7 +136,7 @@ then
   --valid_loader valid_loader --model_v VANV4122GL --upsample vae --lrschedule none --optim_lr 0.0002 --checkpoint \
   --pretrained_model_name pre_train_64-128-256-512_3-4-6-3_8-8-4-4_vae_inferer_valid_loader_PREVANV4121GL_2_20_500_128_True_5_pre_version2__final.pt
 
-elif [ $job -eq 41227 ] && [ $cluster_size -eq 40 ] && [ $freeze -eq 1]
+elif [ $job -eq 41227 ] && [ $cluster_size -eq 40 ] && [ $freeze -eq 1 ]
 then
   PYTHONPATH=. python3 BTCV/main.py --use_ssl_pretrained --workers 8 --base_data ../images/BTCV/Abdomen/RawData/Training \
   --json_list input_list/dataset_BTCV_List.json  --logdir ./runs/BTCV_new/test_log40 --save_checkpoint --max_epochs 5000 \
@@ -145,7 +145,7 @@ then
   --valid_loader valid_loader --model_v VANV4122GL --upsample vae --lrschedule none --optim_lr 0.0002 --checkpoint \
   --pretrained_model_name pre_train_64-128-256-512_3-4-6-3_8-8-4-4_vae_inferer_valid_loader_PREVANV4121GL_2_40_500_128_True_5_pre_version2__final.pt
 
-elif [ $job -eq 41227 ] && [ $cluster_size -eq 80 ] && [ $freeze -eq 1]
+elif [ $job -eq 41227 ] && [ $cluster_size -eq 80 ] && [ $freeze -eq 1 ]
 then
   PYTHONPATH=. python3 BTCV/main.py --use_ssl_pretrained --workers 8 --base_data ../images/BTCV/Abdomen/RawData/Training \
   --json_list input_list/dataset_BTCV_List.json  --logdir ./runs/BTCV_new/test_log80 --save_checkpoint --max_epochs 5000 \
@@ -154,7 +154,7 @@ then
   --valid_loader valid_loader --model_v VANV4122GL --upsample vae --lrschedule none --optim_lr 0.0002 --checkpoint \
   --pretrained_model_name pre_train_64-128-256-512_3-4-6-3_8-8-4-4_vae_inferer_valid_loader_PREVANV4121GL_2_80_500_128_True_5_pre_version2__final.pt
 
-elif [ $job -eq 41229 ] && [ $cluster_size -eq 1 ] && [ $freeze -eq 1]
+elif [ $job -eq 41229 ] && [ $cluster_size -eq 1 ] && [ $freeze -eq 1 ]
 then
   PYTHONPATH=. python3 BTCV/main.py --use_ssl_pretrained --workers 8 --base_data ../images/BTCV/Abdomen/RawData/Training \
   --json_list input_list/dataset_BTCV_List.json  --logdir ./runs/BTCV_new/test_log1 --save_checkpoint --max_epochs 5000 \
@@ -163,7 +163,7 @@ then
   --valid_loader valid_loader --model_v VANV4122GL --upsample vae --lrschedule none --optim_lr 0.0002 --checkpoint \
   --pretrained_model_name pre_train_96-192-384-768_3-3-24-3_8-8-4-4_vae_inferer_valid_loader_PREVANV4121GL_2_1_500_128_True_5_pre_version2__final.pt
 
-elif [ $job -eq 41229 ] && [ $cluster_size -eq 10 ] && [ $freeze -eq 1]
+elif [ $job -eq 41229 ] && [ $cluster_size -eq 10 ] && [ $freeze -eq 1 ]
 then
   PYTHONPATH=. python3 BTCV/main.py --use_ssl_pretrained --workers 8 --base_data ../images/BTCV/Abdomen/RawData/Training \
   --json_list input_list/dataset_BTCV_List.json  --logdir ./runs/BTCV_new/test_log10 --save_checkpoint --max_epochs 5000 \
@@ -172,7 +172,7 @@ then
   --valid_loader valid_loader --model_v VANV4122GL --upsample vae --lrschedule none --optim_lr 0.0002 --checkpoint \
   --pretrained_model_name pre_train_96-192-384-768_3-3-24-3_8-8-4-4_vae_inferer_valid_loader_PREVANV4121GL_2_10_500_128_True_5_pre_version2__final.pt
 
-elif [ $job -eq 41229 ] && [ $cluster_size -eq 20 ] && [ $freeze -eq 1]
+elif [ $job -eq 41229 ] && [ $cluster_size -eq 20 ] && [ $freeze -eq 1 ]
 then
   PYTHONPATH=. python3 BTCV/main.py --use_ssl_pretrained --workers 8 --base_data ../images/BTCV/Abdomen/RawData/Training \
   --json_list input_list/dataset_BTCV_List.json  --logdir ./runs/BTCV_new/test_log20 --save_checkpoint --max_epochs 5000 \
@@ -181,7 +181,7 @@ then
   --valid_loader valid_loader --model_v VANV4122GL --upsample vae --lrschedule none --optim_lr 0.0002 --checkpoint \
   --pretrained_model_name pre_train_96-192-384-768_3-3-24-3_8-8-4-4_vae_inferer_valid_loader_PREVANV4121GL_2_20_500_128_True_5_pre_version2__final.pt
 
-elif [ $job -eq 41229 ] && [ $cluster_size -eq 40 ] && [ $freeze -eq 1]
+elif [ $job -eq 41229 ] && [ $cluster_size -eq 40 ] && [ $freeze -eq 1 ]
 then
   PYTHONPATH=. python3 BTCV/main.py --use_ssl_pretrained --workers 8 --base_data ../images/BTCV/Abdomen/RawData/Training \
   --json_list input_list/dataset_BTCV_List.json  --logdir ./runs/BTCV_new/test_log40 --save_checkpoint --max_epochs 5000 \
@@ -190,7 +190,7 @@ then
   --valid_loader valid_loader --model_v VANV4122GL --upsample vae --lrschedule none --optim_lr 0.0002 --checkpoint \
   --pretrained_model_name pre_train_96-192-384-768_3-3-24-3_8-8-4-4_vae_inferer_valid_loader_PREVANV4121GL_2_40_500_128_True_5_pre_version2__final.pt
 
-elif [ $job -eq 41229 ] && [ $cluster_size -eq 80 ] && [ $freeze -eq 1]
+elif [ $job -eq 41229 ] && [ $cluster_size -eq 80 ] && [ $freeze -eq 1 ]
 then
   PYTHONPATH=. python3 BTCV/main.py --use_ssl_pretrained --workers 8 --base_data ../images/BTCV/Abdomen/RawData/Training \
   --json_list input_list/dataset_BTCV_List.json  --logdir ./runs/BTCV_new/test_log80 --save_checkpoint --max_epochs 5000 \
