da28b7aa6be168ece7aaf7c06f5c9e0b9e6b19fd
amin <amin.karimi1992@gmail.com>
2023-08-04 02:13:42 -0400

    remove log

--- MSD/utils/data_utils.py
+++ MSD/utils/data_utils.py
@@ -154,33 +154,33 @@ def get_loader(args):
 
     train_ds = None
     val_ds = None
-    if args.test_mode:
-        test_transform = transforms.Compose(
-            [
-                transforms.LoadImaged(keys=["image"]),
-                transforms.AddChanneld(keys=["image"]),
-                transforms.Orientationd(keys=["image"], axcodes="RAS"),
-                transforms.Spacingd(
-                    keys=["image"], pixdim=(args.space_x, args.space_y, args.space_z), mode=("bilinear")
-                ),
-                transforms.ScaleIntensityRanged(
-                    keys=["image"], a_min=args.a_min, a_max=args.a_max, b_min=args.b_min, b_max=args.b_max, clip=True
-                ),
-                transforms.ToTensord(keys=["image"]),
-            ]
-        )
-        test_ds = data.Dataset(data=test_files, transform=test_transform)
-        test_sampler = Sampler(test_ds, shuffle=False) if args.distributed else None
-        test_loader = data.DataLoader(
-            test_ds,
-            batch_size=1,
-            shuffle=False,
-            num_workers=args.workers,
-            sampler=test_sampler,
-            pin_memory=True
-        )
-        loader = test_loader
-    elif args.val_mode:
+    # if args.test_mode:
+    #     test_transform = transforms.Compose(
+    #         [
+    #             transforms.LoadImaged(keys=["image"]),
+    #             transforms.AddChanneld(keys=["image"]),
+    #             transforms.Orientationd(keys=["image"], axcodes="RAS"),
+    #             transforms.Spacingd(
+    #                 keys=["image"], pixdim=(args.space_x, args.space_y, args.space_z), mode=("bilinear")
+    #             ),
+    #             transforms.ScaleIntensityRanged(
+    #                 keys=["image"], a_min=args.a_min, a_max=args.a_max, b_min=args.b_min, b_max=args.b_max, clip=True
+    #             ),
+    #             transforms.ToTensord(keys=["image"]),
+    #         ]
+    #     )
+    #     test_ds = data.Dataset(data=test_files, transform=test_transform)
+    #     test_sampler = Sampler(test_ds, shuffle=False) if args.distributed else None
+    #     test_loader = data.DataLoader(
+    #         test_ds,
+    #         batch_size=1,
+    #         shuffle=False,
+    #         num_workers=args.workers,
+    #         sampler=test_sampler,
+    #         pin_memory=True
+    #     )
+    #     loader = test_loader
+    if args.val_mode or args.test_mode:
         extra_val_ds = data.Dataset(data=extra_val, transform=val_transform)
         val_sampler = Sampler(extra_val_ds, shuffle=False) if args.distributed else None
         extra_val_loader = data.DataLoader(
