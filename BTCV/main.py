51368c7c0c2f51d6098b8b66964613a59004ae3e
amin <amin.karimi1992@gmail.com>
2023-09-28 16:07:57 -0400

    change port

--- BTCV/main.py
+++ BTCV/main.py
@@ -223,6 +223,6 @@ if __name__ == "__main__":
     job_id = os.environ.get('SLURM_JOB_ID', None)
     os.environ['MASTER_ADDR'] = 'localhost'
     # os.environ['MASTER_PORT'] = '12346'
-    master_port = int(job_id) + 12345  
+    master_port = int(job_id) % 20000  + 12345
     os.environ['MASTER_PORT'] = str(master_port)
     main()
