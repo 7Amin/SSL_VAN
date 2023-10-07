6d7752732313705708498fe8c6adb4027830c677
Amin Karimi Monsefi <amin.karimi1992@gmail.com>
2023-10-06 23:41:06 -0400

    adding nn.SyncBatchNorm(hidden_dim),

--- commons/models/util_models/van_3d.py
+++ commons/models/util_models/van_3d.py
@@ -58,12 +58,14 @@ class Block3D(nn.Module):
                  norm_cfg=dict(type='SyncBN', requires_grad=True)):
         super().__init__()
         # self.norm1 = build_norm_layer(norm_cfg, dim)[1]
-        self.norm1 = nn.BatchNorm3d(dim)
+        # self.norm1 = nn.BatchNorm3d(dim)
+        self.norm1 = nn.SyncBatchNorm(dim),
         self.attn = SpatialAttention3D(dim)
         self.drop_path = DropPath(drop_path) if drop_path > 0. else nn.Identity()
 
         # self.norm2 = build_norm_layer(norm_cfg, dim)[1]
-        self.norm2 = nn.BatchNorm3d(dim)
+        # self.norm2 = nn.BatchNorm3d(dim)
+        self.norm2 = nn.SyncBatchNorm(dim),
         mlp_hidden_dim = int(dim * mlp_ratio)
         self.mlp = Mlp(in_features=dim, hidden_features=mlp_hidden_dim,
                        act_layer=act_layer, drop=drop, linear=linear)
@@ -107,7 +109,8 @@ class OverlapPatchEmbed3D(nn.Module):
         #  todo remove
         # norm_cfg = dict(type='BN', requires_grad=True)
         # self.norm = build_norm_layer(norm_cfg, embed_dim)[1]
-        self.norm = nn.BatchNorm3d(embed_dim)
+        # self.norm = nn.BatchNorm3d(embed_dim)
+        self.norm = nn.SyncBatchNorm(embed_dim),
 
     def forward(self, x):
 
