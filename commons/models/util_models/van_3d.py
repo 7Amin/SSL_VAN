7e77de0d1cecc400f4dc7fc82f9e367a3a071317
Amin Karimi Monsefi <amin.karimi1992@gmail.com>
2023-10-06 23:52:08 -0400

    fix bug

--- commons/models/util_models/van_3d.py
+++ commons/models/util_models/van_3d.py
@@ -59,13 +59,13 @@ class Block3D(nn.Module):
         super().__init__()
         # self.norm1 = build_norm_layer(norm_cfg, dim)[1]
         # self.norm1 = nn.BatchNorm3d(dim)
-        self.norm1 = nn.SyncBatchNorm(dim),
+        self.norm1 = nn.SyncBatchNorm(dim)
         self.attn = SpatialAttention3D(dim)
         self.drop_path = DropPath(drop_path) if drop_path > 0. else nn.Identity()
 
         # self.norm2 = build_norm_layer(norm_cfg, dim)[1]
         # self.norm2 = nn.BatchNorm3d(dim)
-        self.norm2 = nn.SyncBatchNorm(dim),
+        self.norm2 = nn.SyncBatchNorm(dim)
         mlp_hidden_dim = int(dim * mlp_ratio)
         self.mlp = Mlp(in_features=dim, hidden_features=mlp_hidden_dim,
                        act_layer=act_layer, drop=drop, linear=linear)
